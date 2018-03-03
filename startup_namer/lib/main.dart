3796cbe94bdababdfc08e623f0cf937331473dca
carojaspaz <carojaspaz@gmail.com>
2018-03-02 22:12:51 -0500

    201803022212

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -7,7 +7,10 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){    
     return new MaterialApp(
-      title: "Welcome to Flutter",
+      title: "Startup Name Generator",
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: new RandomWords(),
     );
   }
@@ -20,25 +23,72 @@ class RandomWords extends StatefulWidget{
 
 class RandomWordsState extends State<RandomWords>{  
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
 
+  void _pushSaved(){
+        Navigator.of(context).push(
+          new MaterialPageRoute(
+            builder: (context){
+              final tiles = _saved.map(
+                (pair){
+                  return new ListTile(
+                    title: new Text(pair.asPascalCase,
+                    style: _biggerFont
+                    ),
+                  );
+                },
+              );
+              final divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles
+              )
+              .toList();
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: new Text('Saved Suggestion'),
+                ),
+                body: new ListView(children : divided),
+              );
+            },
+          )
+        );
+      }
+
+  @override  
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
-    );
+      trailing: new Icon(
+        alreadySaved? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          } else  {
+            _saved.add(pair);
+          }
+        });
+      });        
   }
 
   Widget _buildSuggestions(){
